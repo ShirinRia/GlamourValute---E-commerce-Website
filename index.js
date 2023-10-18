23079788b5046caa9acf32a311f356d8a4841416
SHIRIN <efshitaria123@gmail.com>
2023-10-18 14:21:28 +0600

    POST functionality for adding new product data to mongodb

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const {
+  MongoClient,
+  ServerApiVersion
+} = require('mongodb');
 const express = require('express')
 var cors = require('cors')
 require('dotenv').config()
@@ -5,12 +9,52 @@ var app = express()
 
 app.use(cors())
 app.use(express.json())
-const port = process.env.PORT||5000
+const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+USERNAME = process.env.S3_BUCKET
+PASS = process.env.SECRET_KEY
+const uri = `mongodb+srv://${USERNAME}:${PASS}@cluster0.xrp2z6o.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const productcollection = client.db("product").collection("productdata");
+    
+    // add new product to database
+    app.post('/products', async (req, res) => {
+      const products = req.body
+      console.log(products)
+      const result = await productcollection.insertOne(products);
+      res.send(result)
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({
+      ping: 1
+    });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
